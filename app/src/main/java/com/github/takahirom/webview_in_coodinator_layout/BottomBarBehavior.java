2aeb5a79f331d56eb816692e6d3225ff6334a1c4
Matej Hrazdira <matejhrazdira@gmail.com>
2019-11-15 20:48:31 +0100

    Update for androidx.

--- app/src/main/java/com/github/takahirom/webview_in_coodinator_layout/BottomBarBehavior.java
+++ app/src/main/java/com/github/takahirom/webview_in_coodinator_layout/BottomBarBehavior.java
@@ -17,9 +17,9 @@
 package com.github.takahirom.webview_in_coodinator_layout;
 
 import android.content.Context;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.v4.view.ViewCompat;
+import com.google.android.material.appbar.AppBarLayout;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
